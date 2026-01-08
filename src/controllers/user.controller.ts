686c294432f402d4934df64b13c36b8547e478b2
Samitha Gunarathna <samitha@Samithas-MacBook-Air.local>
2026-01-08 21:06:53 +1100

    user registration and login apis

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { addNewUser } from '../services/user.service';
+import { sendResponse } from '../utils/sendResponse';
 
 export const registerUser = async (req: Request, res: Response) => {
     try {
@@ -7,13 +8,22 @@ export const registerUser = async (req: Request, res: Response) => {
 
         // Validate input
         if (!email || !password || !fullName || !userName) {
-            return res.status(400).json({ message: 'Missing required fields' });
+            return sendResponse(res, 400, 'All fields are required', false);
         }
 
-        await addNewUser(req.body);
+        const data = await addNewUser(req.body);
 
-        res.status(201).json({ message: 'User registered successfully' });
-    } catch (error) {
-        res.status(500).json({ message: 'Registration failed', error });
+        return sendResponse(res, 200, {
+            id: data._id,
+            username: data.userName,
+        });
+
+    } catch (error: any) {
+
+        if (error.message ==='USER_ALREADY_EXISTS') {
+            return sendResponse(res, 409, 'User already exists', false);
+        }
+
+        return sendResponse(res, 500, 'Registration failed', false);
     }
 };
